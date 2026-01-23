9052ae5b1a306fa0878ebaf8bf6e94f7af9990d2
Vikri Permana <vikripermana91@gmail.com>
2026-01-23 15:55:06 +0700

    🔧 : Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import glsl from "vite-plugin-glsl";
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
+  build: {
+    target: "es2022",
+    cssTarget: "es2022",
+  },
   plugins: [glsl(), wasm(), cloudflare()],
 });
